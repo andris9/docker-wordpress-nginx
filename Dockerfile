6429b4f277796ff9511faa09591c287a1899196e
Andris Reinman <andris@kreata.ee>
2014-01-24 15:48:13 +0200

    New features: auto upgrade through ssh2, different user permissions for php, allow larger uploads

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM ubuntu
-MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+
+# Forked from https://github.com/eugeneware/docker-wordpress-nginx
+# MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+
+MAINTAINER Andris Reinman <andris@kreata.ee>
+
 RUN echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list
 RUN apt-get update
 RUN apt-get -y upgrade
@@ -9,24 +14,34 @@ RUN dpkg-divert --local --rename --add /sbin/initctl
 RUN ln -s /bin/true /sbin/initctl
 
 # Basic Requirements
-RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
+RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip openssh-server vim
 
 # Wordpress Requirements
-RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl libssh2-php
+
+# Create directory for sshd and set locale
+RUN mkdir -p /var/run/sshd && locale-gen en_US.utf8 && echo 'LC_ALL="en_US.utf8"' > /etc/environment
 
 # mysql config
-RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+RUN sed -i -e "s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+RUN sed -i -e "s/^syslog/log-error=error.log/" /etc/mysql/conf.d/mysqld_safe_syslog.cnf
 
 # nginx config
-RUN sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
-# since 'upload_max_filesize = 2M' in /etc/php5/fpm/php.ini
-RUN sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 3m/" /etc/nginx/nginx.conf
+RUN sed -i -e "s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
+# since 'upload_max_filesize = 10M' in /etc/php5/fpm/php.ini
+RUN sed -i -e "s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 10m/;s/# server_tokens on;/server_tokens off;/" /etc/nginx/nginx.conf
+
 RUN echo "daemon off;" >> /etc/nginx/nginx.conf
 
 # php-fpm config
-RUN sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
+# "allow_url_fopen" must be "On" for WordPress auto upgrade
+RUN sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g;s/upload_max_filesize = 2M/upload_max_filesize = 10M/;s/post_max_size = 8M/post_max_size = 10M/;s/expose_php = On/expose_php = Off/;s/max_execution_time = 30/max_execution_time = 60/;s/;ignore_user_abort = On/ignore_user_abort = Off/" /etc/php5/fpm/php.ini
 RUN sed -i -e "s/;daemonize\s*=\s*yes/daemonize = no/g" /etc/php5/fpm/php-fpm.conf
 RUN find /etc/php5/cli/conf.d/ -name "*.ini" -exec sed -i -re 's/^(\s*)#(.*)/\1;\2/g' {} \;
+RUN echo "php_admin_value[sendmail_path] = /usr/bin/sendmail -t -i" >> /etc/php5/fpm/pool.d/www.conf
+
+# User for the blog
+RUN useradd -s /bin/bash -d /home/wordpress -m wordpress && usermod -aG www-data wordpress
 
 # nginx site conf
 ADD ./nginx-site.conf /etc/nginx/sites-available/default
@@ -36,18 +51,19 @@ RUN /usr/bin/easy_install supervisor
 ADD ./supervisord.conf /etc/supervisord.conf
 
 # Install Wordpress
-ADD http://wordpress.org/latest.tar.gz /wordpress.tar.gz
+ADD http://et.wordpress.org/latest-et.tar.gz /wordpress.tar.gz
 RUN tar xvzf /wordpress.tar.gz -C /usr/share/nginx
 RUN mv /usr/share/nginx/www/5* /usr/share/nginx/wordpress
 RUN rm -rf /usr/share/nginx/www
 RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
-RUN chown -R www-data:www-data /usr/share/nginx/www
+RUN chown -R wordpress:wordpress /usr/share/nginx/www
 
 # Wordpress Initialization and Startup Script
 ADD ./start.sh /start.sh
 RUN chmod 755 /start.sh
 
 # private expose
+EXPOSE 22
 EXPOSE 80
 
 CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,54 +1,104 @@
 #!/bin/bash
+
+# DB_PASSWORD=$(awk -F "'" '/DB_PASSWORD/ {print $4}' /var/data/sources/wp-config.php)
+# DB_USER=$(awk -F "'" '/DB_USER/ {print $4}' /var/data/sources/wp-config.php)
+# DB_NAME=$(awk -F "'" '/DB_NAME/ {print $4}' /var/data/sources/wp-config.php)
+
 if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe & 
+  /usr/bin/mysqld_safe &
   sleep 10s
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
   WORDPRESS_DB="wordpress"
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs. 
-  echo mysql root password: $MYSQL_PASSWORD
-  echo wordpress password: $WORDPRESS_PASSWORD
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
+  ROOT_PASSWORD=`pwgen -c -n -1 12`
+  USER_PASSWORD=`pwgen -c -n -1 12`
+
+  WORDPRESS_ROOT="/usr/share/nginx/www"
+  WORDPRESS_UPLOADS="uploads"
+
+  mkdir -p "$WORDPRESS_ROOT/$WORDPRESS_UPLOADS"
+
+  ln -s $WORDPRESS_ROOT /home/wordpress/wordpress
+
+  echo "root:$ROOT_PASSWORD" | chpasswd
+  echo "wordpress:$USER_PASSWORD" | chpasswd
+
+  # Expose passwords in logs
+  echo root password: $ROOT_PASSWORD
+  echo user password: $USER_PASSWORD
+
+  # Escape any unsopperted chars, eg "/"
+  SAFE_WORDPRESS_UPLOADS=$(printf '%s\n' "$WORDPRESS_UPLOADS" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
+  SAFE_WORDPRESS_ROOT=$(printf '%s\n' "$WORDPRESS_ROOT/" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
 
   sed -e "s/database_name_here/$WORDPRESS_DB/
-  s/username_here/$WORDPRESS_DB/
-  s/password_here/$WORDPRESS_PASSWORD/
-  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+s/username_here/$WORDPRESS_DB/
+s/password_here/$USER_PASSWORD/
+/'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'WP_DEBUG'/{G;s/$/define( 'UPLOADS', '$SAFE_WORDPRESS_UPLOADS' );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FS_METHOD', 'ssh2' );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_BASE', '$SAFE_WORDPRESS_ROOT' );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_USER', 'wordpress' );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_HOST', 'localhost' );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_SSL', true );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_PUBKEY', false );/;}
+/'WP_DEBUG'/{G;s/$/define( 'FTP_PRIKEY', false );/;}
+/'WP_DEBUG'/{G;s/$/define( 'DISALLOW_FILE_EDIT', true );/;}" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
 
   # Download nginx helper plugin
   curl -O `curl -i -s http://wordpress.org/plugins/nginx-helper/ | egrep -o "http://downloads.wordpress.org/plugin/[^']+"`
   unzip nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
-  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
+  chown -R wordpress:wordpress /usr/share/nginx/www/wp-content/plugins/nginx-helper
 
   # Activate nginx plugin and set up pretty permalink structure once logged in
   cat << ENDL >> /usr/share/nginx/www/wp-config.php
 \$plugins = get_option( 'active_plugins' );
 if ( count( \$plugins ) === 0 ) {
-  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-  \$wp_rewrite->set_permalink_structure( '/%postname%/' );
-  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
-  foreach ( \$pluginsToActivate as \$plugin ) {
-    if ( !in_array( \$plugin, \$plugins ) ) {
-      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
+    require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+    \$wp_rewrite->set_permalink_structure( '/%postname%/' );
+    \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+    foreach ( \$pluginsToActivate as \$plugin ) {
+        if ( !in_array( \$plugin, \$plugins ) ) {
+            activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
+        }
     }
-  }
 }
 ENDL
 
-  chown www-data:www-data /usr/share/nginx/www/wp-config.php
+  chown wordpress:wordpress -R /usr/share/nginx/www
+  chown www-data:www-data -R "$WORDPRESS_ROOT/$WORDPRESS_UPLOADS"
 
-  mysqladmin -u root password $MYSQL_PASSWORD 
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
+  find "$WORDPRESS_ROOT" -type d -exec chmod 755 {} \;
+  find "$WORDPRESS_ROOT" -type f -exec chmod 644 {} \;
+
+  # create dummy sendmail client
+  SENDMAIL_LOG="/var/log/sendmail.log"
+  echo "#!/bin/bash
+
+SENDMAIL_LOG=\"$SENDMAIL_LOG\"
+
+echo \"[`date`]\" >> \$SENDMAIL_LOG
+echo \"\$0 \$*\" >> \$SENDMAIL_LOG
+
+while read line
+do
+  echo \"\$line\" >> \$SENDMAIL_LOG
+done < /proc/\${\$}/fd/0
+
+echo \"\" >> \$SENDMAIL_LOG" > /usr/bin/sendmail
+  chmod +x /usr/bin/sendmail
+  touch $SENDMAIL_LOG
+  chown www-data:www-data $SENDMAIL_LOG
+
+  mysqladmin -u root password $ROOT_PASSWORD
+  mysql -uroot -p$ROOT_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$USER_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
 fi
 
