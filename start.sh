19b4b99931bb4bc9b57b1fe31c0708725994b244
Andris Reinman <andris@kreata.ee>
2014-01-27 12:47:08 +0200

    Added disable_functions list (disable shell functions)

--- start.sh
+++ start.sh
@@ -69,6 +69,7 @@ s/password_here/$MYSQL_USER_PASSWORD/
 
   # Activate nginx plugin and set up pretty permalink structure once logged in
   cat << ENDL >> /usr/share/nginx/www/wp-config.php
+
 \$plugins = get_option( 'active_plugins' );
 if ( count( \$plugins ) === 0 ) {
     require_once(ABSPATH .'/wp-admin/includes/plugin.php');
